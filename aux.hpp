01526b10b2edc862240f9998e3876376edb4808a
MikhailBekhovskiy <mbexovskij@gmail.com>
2019-12-30 18:02:32 +0300

    added multiplication to support header

--- aux.hpp
+++ aux.hpp
@@ -16,6 +16,23 @@ double** newMatrix (int n, int m){
     }
 }
 
+double** matrMult (double** A, double** B, int n, int m, int k){
+    double **result = new double*[n];
+    for (int i = 0; i<n;++i){
+        result[i] = new double[k];
+        for (int j =0;j<k;++j){
+            result[i][j] = 0;
+        }
+    }
+    for (int i =0;i<n;++i){
+        for (int j = 0;j<k;++j){
+            for (int l = 0;l<m;++l){
+                result[i][j]+=A[i][l]*B[l][j];
+            }
+        }
+    }
+    return result;
+}
 
 
 
